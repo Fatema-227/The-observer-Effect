13f32382d6ecdd275101a6f504681f4d0f345cd8
Fatema-227 <fat00m.alshughul.2oo5@gmail.com>
2025-09-14 13:08:45 +0300

    done

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 console.log("JS working!")
 
-const cardSymbols = ["./data"]
+let cardData = []
 let cards = []
-let flippedCard = []
-let matched = 0
+let flippedCards = []
+let matchedPairs = 0
 let attempts = 0
 let observationTimer = null
 let remainingObservationTime = 30
 let gameActive = false
 
+//Download card data from a file:
+fetch("./data/cards.json")
+  .then((response) => response.json())
+  .then((data) => {
+    cardData = data
+    startGame()
+  })
+
 //start Game:
 function startGame() {
-  cards = shuffle([...cardSymbols, ...cardSymbols])
+  const pairedCards = []
+  cardData.forEach((cards) => {
+    pairedCards.push({ ...cards })
+    pairedCards.push({ ...cards })
+  })
+  cards = shuffle(pairedCards)
   createCards(cards)
   revealAllCards()
-  gameStarted = false
+  gameActive = false
   matched = []
   attempts = 0
+  updateUI()
+}
+
+//قلب البطاقات
+function handleCardClick(cards) {
+  if (!gameStarted) return
+  if (cards.classList.contains("flippedCard") || flippedCard.length === 2)
+    return
+
+  card.classList.add("flippedCard")
+  flippedCard.push(cards)
+
+  if (flippedCard.length === 2) {
+    attempts++
+    updateUI()
+    setTimeout(checkMatch, 600)
+  }
 }
