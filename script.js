9b726700c6cc78f4326f28c88e1c578eb2f17be9
Fatema-227 <fat00m.alshughul.2oo5@gmail.com>
2025-09-14 11:55:35 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("JS working!")
+
+const cardSymbols = ["./data"]
+let cards = []
+let flippedCard = []
+let matched = 0
+let attempts = 0
+let observationTimer = null
+let remainingObservationTime = 30
+let gameActive = false
+
+//start Game:
+function startGame() {
+  cards = shuffle([...cardSymbols, ...cardSymbols])
+  createCards(cards)
+  revealAllCards()
+  gameStarted = false
+  matched = []
+  attempts = 0
+}
